5c44eb58ddb7f4af91acb157f91cf044e262a44e
Anna Agronomovs <a.agronomovs@yandex.ru>
2022-02-17 19:31:44 +0300

    change port to 3000

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const routerUser = require('./routes/users');
 const routerCards = require('./routes/cards');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 
 const app = express();
