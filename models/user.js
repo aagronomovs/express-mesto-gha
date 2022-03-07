0fdd94b1e97bda32b891e9f95caa8809e1110ff3
Anna Agronomovs <a.agronomovs@yandex.ru>
2022-03-07 12:14:34 +0300

    add email and password to user model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 // models/user.js
 const mongoose = require('mongoose');
+//const validator = require('validator');
+const { default: isEmail } = require('validator/lib/isEmail');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: { // у пользователя есть имя — опишем требования к имени в схеме:
@@ -7,18 +10,58 @@ const userSchema = new mongoose.Schema({
     required: true, // оно должно быть у каждого пользователя, так что имя — обязательное поле
     minlength: 2, // минимальная длина имени — 2 символа
     maxlength: 30, // а максимальная — 30 символов
+    default: 'Жак-Ив Кусто',
   },
- about: {
+  about: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 30,
- },
- avatar: {
-  type: String,
-  required: true,
-},
+    default: 'Исследователь',
+  },
+  avatar: {
+    type: String,
+    required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator(v) {
+        return /(:?(?:https?:\/\/)?(?:www\.)?)?[-a-z0-9]+\.\w/gi.test(v);
+      },
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: "Неправильный формат email"
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  }
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+
+          return user; // теперь user доступен
+        });
+    });
+};
 
 // создаём модель и экспортируем её
 module.exports = mongoose.model('user', userSchema);
